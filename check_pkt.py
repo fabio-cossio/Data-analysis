6276fc8e5180e3626e8dc22e04f777760b62c7b5
fabio-cossio <Fabio@srv-lab.fe.infn.it>
2020-04-23 17:54:33 +0200

    log files addded

--- check_pkt.py
+++ check_pkt.py
@@ -48,6 +48,7 @@ path = "{}/CHECK_PACKETS/{}".format(home_folder, RUN)
 if not(os.path.isdir(path)):
         os.mkdir(path)
 
+f = open("{}/RUN_{}_log.txt".format(path, RUN), "w")
 
 #################################################################################
 #################################################################################
@@ -63,8 +64,8 @@ chain.Add("{}/Sub_RUN_dec_*.root".format(data_path))
 #################################################################################
 
 
-print("RUN = {}".format(RUN))
-
+print("\nRUN = {}".format(RUN))
+f.write("\nRUN = {}\n".format(RUN))
 
 
 subrun_max = 100000
@@ -73,6 +74,7 @@ chain.Draw("subRunNo>>h1", "", "colz")
 bin = h1.FindLastBinAbove(0, 1)
 subrun_max = int(h1.GetXaxis().GetBinCenter(bin)) + 2
 print("subRunNo MAX = {}".format(subrun_max))
+f.write("subRunNo MAX = {}\n".format(subrun_max))
 h1.Delete()
 
 
@@ -82,11 +84,14 @@ chain.Draw("count>>h1", "", "colz")
 bin = h1.FindLastBinAbove(0, 1)
 l1count_max = int(h1.GetXaxis().GetBinCenter(bin)) + 10
 print("L1 count MAX = {}".format(l1count_max))
+f.write("L1 count MAX = {}\n".format(l1count_max))
 h1.Delete()
 
 
 TOT = int(chain.GetEntries())
 print("TOTAL ENTRIES = {}".format(TOT))
+f.write("TOTAL ENTRIES = {}\n".format(TOT))
+
 
 
 
@@ -102,6 +107,7 @@ chain.Draw("count>>h1", condition, "colz")
 
 TOT1 = int(h1.GetEntries())
 print("TOTAL ENTRIES = {} ({} spourious removed, {:.2f}%)".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
+f.write("TOTAL ENTRIES = {} ({} spourious removed, {:.2f}%)\n".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
 
 c1.Update()
 c1.SaveAs("{}/count.pdf".format(path))
@@ -115,6 +121,9 @@ c1.Close()
 #################################################################################
 ## 2. PACCHETTI SFASATI
 
+print("\nANALYSIS ON L1TS_MIN_TCOARSE ERRORS")
+f.write("\nANALYSIS ON L1TS_MIN_TCOARSE ERRORS\n")
+
 condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse>1299 && l1ts_min_tcoarse<1567)"
 h1 = ROOT.TH1D("h1", condition, 100000, 0, 100000)
 h1.GetXaxis().SetTitle("count")
@@ -123,13 +132,16 @@ c1 = ROOT.TCanvas("c11", "c11", 100, 100, 1600, 1000)
 chain.Draw("count>>h1", condition, "colz")
 
 TOT2 = int(h1.GetEntries())
-print(" GOOD ENTRIES = {} ({:.2f}%)".format( TOT2, TOT2/float(TOT1)*100 ) )
+print("GOOD ENTRIES = {} ({:.2f}%)".format( TOT2, TOT2/float(TOT1)*100 ) )
+f.write("GOOD ENTRIES = {} ({:.2f}%)\n".format( TOT2, TOT2/float(TOT1)*100 ) )
 
 c1.Update()
 c1.SaveAs("{}/good.pdf".format(path))
 h1.Delete()
 c1.Close()
 
+print(" BAD ENTRIES = {} ({:.2f}%)".format( TOT1-TOT2, (TOT1-TOT2)/float(TOT1)*100 ) )
+f.write(" BAD ENTRIES = {} ({:.2f}%)\n".format( TOT1-TOT2, (TOT1-TOT2)/float(TOT1)*100 ) )
 
 
 
@@ -151,17 +163,51 @@ c1.Close()
 
 
 
+#################################################################################
+## 4. PACCHETTI SFASATI (errors on TIGER 0-3 and 4-7)
+
+condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && tiger<4"
+h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, 15, 0, 15)
+h1.GetXaxis().SetTitle("count")
+h1.GetYaxis().SetTitle("gemroc")
+c1 = ROOT.TCanvas("c11", "c11", 100, 100, 1800, 1200)
+chain.Draw("gemroc:count>>h1", condition, "*")
+print("l1ts_min_tcoarse errors from TIGER 0-3 = {} ({:.2f}%)".format( int(h1.GetEntries()), h1.GetEntries()/(TOT1-TOT2)*100 ) )
+f.write("l1ts_min_tcoarse errors from TIGER 0-3 = {} ({:.2f}%)\n".format( int(h1.GetEntries()), h1.GetEntries()/(TOT1-TOT2)*100 ) )
+
+c1.Update()
+c1.SaveAs("{}/COUNTvsGEMROC_TIGER03.pdf".format(path))
+h1.Delete()
+c1.Close()
 
 
+condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && tiger>3"
+h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, 15, 0, 15)
+h1.GetXaxis().SetTitle("count")
+h1.GetYaxis().SetTitle("gemroc")
+c1 = ROOT.TCanvas("c11", "c11", 100, 100, 1800, 1200)
+chain.Draw("gemroc:count>>h1", condition, "*")
+print("l1ts_min_tcoarse errors from TIGER 4-7 = {} ({:.2f}%)".format( int(h1.GetEntries()), h1.GetEntries()/(TOT1-TOT2)*100 ) )
+f.write("l1ts_min_tcoarse errors from TIGER 4-7 = {} ({:.2f}%)\n".format( int(h1.GetEntries()), h1.GetEntries()/(TOT1-TOT2)*100 ) )
+
+c1.Update()
+c1.SaveAs("{}/COUNTvsGEMROC_TIGER47.pdf".format(path))
+h1.Delete()
+c1.Close()
+
+
+print("\n")
+f.write("\n")
 
 #################################################################################
-## 4. GEMROC
+## 5. GEMROC
 
 for roc in range(0, 14):
         if roc == 11:
                 continue
         else:
                 print ("Analyzing GEMROC {}...".format(roc))
+                f.write("Analyzing GEMROC {}...\n".format(roc))
 
 
                 condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && gemroc=={}".format(roc)
@@ -186,8 +232,9 @@ for roc in range(0, 14):
 #################################################################################
 
 
+print("\nDone\n\n")
+f.write("\nDone\n\n")
 
-
-
+f.close()
 
 
