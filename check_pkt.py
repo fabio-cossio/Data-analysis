0a3b1bf483872e3077f935ff3aecb79d9e93023d
fabio-cossio <Fabio@srv-lab.fe.infn.it>
2020-04-24 12:17:15 +0200

    new plots

--- check_pkt.py
+++ check_pkt.py
@@ -73,8 +73,8 @@ h1 = ROOT.TH1D("h1", "h1", subrun_max, 0, subrun_max)
 chain.Draw("subRunNo>>h1", "", "colz")
 bin = h1.FindLastBinAbove(0, 1)
 subrun_max = int(h1.GetXaxis().GetBinCenter(bin)) + 2
-print("subRunNo MAX = {}".format(subrun_max))
-f.write("subRunNo MAX = {}\n".format(subrun_max))
+print("subRunNo MAX = {}".format(subrun_max-2))
+f.write("subRunNo MAX = {}\n".format(subrun_max-2))
 h1.Delete()
 
 
@@ -83,8 +83,8 @@ h1 = ROOT.TH1D("h1", "h1", l1count_max, 0, l1count_max)
 chain.Draw("count>>h1", "", "colz")
 bin = h1.FindLastBinAbove(0, 1)
 l1count_max = int(h1.GetXaxis().GetBinCenter(bin)) + 10
-print("L1 count MAX = {}".format(l1count_max))
-f.write("L1 count MAX = {}\n".format(l1count_max))
+print("L1 count MAX = {}".format(l1count_max-10))
+f.write("L1 count MAX = {}\n".format(l1count_max-10))
 h1.Delete()
 
 
@@ -99,15 +99,15 @@ f.write("TOTAL ENTRIES = {}\n".format(TOT))
 ## 1. DATI SPURI
 
 condition = "(delta_coarse==25 || delta_coarse==26)"
-h1 = ROOT.TH1D("h1", "h1", 100000, 0, 100000)
+h1 = ROOT.TH1D("h1", condition, l1count_max, 0, l1count_max)
 h1.GetXaxis().SetTitle("count")
 h1.GetYaxis().SetTitle("N")
 c1 = ROOT.TCanvas("c11", "c11", 100, 100, 1600, 1000)
 chain.Draw("count>>h1", condition, "colz")
 
 TOT1 = int(h1.GetEntries())
-print("TOTAL ENTRIES = {} ({} spourious removed, {:.2f}%)".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
-f.write("TOTAL ENTRIES = {} ({} spourious removed, {:.2f}%)\n".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
+print("TOTAL ENTRIES = {} ({} spurious removed, {:.2f}%)".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
+f.write("TOTAL ENTRIES = {} ({} spurious removed, {:.2f}%)\n".format( TOT1, TOT-TOT1, (TOT-TOT1)/float(TOT)*100 ) )
 
 c1.Update()
 c1.SaveAs("{}/count.pdf".format(path))
@@ -125,7 +125,7 @@ print("\nANALYSIS ON L1TS_MIN_TCOARSE ERRORS")
 f.write("\nANALYSIS ON L1TS_MIN_TCOARSE ERRORS\n")
 
 condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse>1299 && l1ts_min_tcoarse<1567)"
-h1 = ROOT.TH1D("h1", condition, 100000, 0, 100000)
+h1 = ROOT.TH1D("h1", condition, l1count_max, 0, l1count_max)
 h1.GetXaxis().SetTitle("count")
 h1.GetYaxis().SetTitle("N")
 c1 = ROOT.TCanvas("c11", "c11", 100, 100, 1600, 1000)
@@ -145,10 +145,11 @@ f.write(" BAD ENTRIES = {} ({:.2f}%)\n".format( TOT1-TOT2, (TOT1-TOT2)/float(TOT
 
 
 
+
 #################################################################################
 ## 3. PACCHETTI SFASATI (errors)
 
-condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567)"
+condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1300 || l1ts_min_tcoarse>1566)"
 h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, 15, 0, 15)
 h1.GetXaxis().SetTitle("count")
 h1.GetYaxis().SetTitle("gemroc")
@@ -166,7 +167,7 @@ c1.Close()
 #################################################################################
 ## 4. PACCHETTI SFASATI (errors on TIGER 0-3 and 4-7)
 
-condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && tiger<4"
+condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1300 || l1ts_min_tcoarse>1566) && tiger<4"
 h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, 15, 0, 15)
 h1.GetXaxis().SetTitle("count")
 h1.GetYaxis().SetTitle("gemroc")
@@ -181,7 +182,7 @@ h1.Delete()
 c1.Close()
 
 
-condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && tiger>3"
+condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1300 || l1ts_min_tcoarse>1566) && tiger>3"
 h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, 15, 0, 15)
 h1.GetXaxis().SetTitle("count")
 h1.GetYaxis().SetTitle("gemroc")
@@ -199,6 +200,9 @@ c1.Close()
 print("\n")
 f.write("\n")
 
+
+
+
 #################################################################################
 ## 5. GEMROC
 
@@ -210,7 +214,7 @@ for roc in range(0, 14):
                 f.write("Analyzing GEMROC {}...\n".format(roc))
 
 
-                condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1299 || l1ts_min_tcoarse>1567) && gemroc=={}".format(roc)
+                condition = "(delta_coarse==25 || delta_coarse==26) && (l1ts_min_tcoarse<1300 || l1ts_min_tcoarse>1566) && gemroc=={}".format(roc)
                 h1 = ROOT.TH2D("h1", condition, l1count_max, 0, l1count_max, subrun_max, 0, subrun_max)
                 h1.GetXaxis().SetTitle("count")
                 h1.GetYaxis().SetTitle("subRunNo")
